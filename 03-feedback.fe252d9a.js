61fec44528942d1ac704bdb536dbab19b0499829
Pavel-Zolenko <107986786+Pavel-Zolenko@users.noreply.github.com>
2022-11-08 18:54:29 +0000

    Deploying to gh-pages from @ Pavel-Zolenko/goit-js-hw-08_2@8186b1f90955fa28f28c7f177cb9a264d80f5967 🚀

--- 03-feedback.fe252d9a.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form").addEventListener("input",(function(e){var t=e.currentTarget.elements,a=t.email,r=t.message,m={email:a.value,message:r.value};localStorage.setItem("feedback-form-state",JSON.stringify(m))}));
-//# sourceMappingURL=03-feedback.fe252d9a.js.map
