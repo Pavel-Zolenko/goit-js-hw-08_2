8186b1f90955fa28f28c7f177cb9a264d80f5967
Pavel-Zolenko <pobedu8@gmail.com>
2022-11-08 20:54:01 +0200

    last

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,48 @@
+const throttle = require("lodash.throttle");
+
 const form = document.querySelector(".feedback-form");
 
-form.addEventListener("input", onInputMessage);
-function onInputMessage(ent) {
-        const {
+form.addEventListener("input", throttle(onInputMessage, 500));
+const formData = {}
+
+function onInputMessage(evt) {
+    //     const {
+    // elements: { email, message }
+    // } = evt.currentTarget;
+    // const data = {
+    //     email: email.value,
+    //     message: message.value,
+    // };
+    
+    formData[evt.target.name] = evt.target.value;
+      
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+            
+}
+getText()
+function getText() {
+    const getItemData = JSON.parse(localStorage.getItem('feedback-form-state')) 
+    
+    if (getItemData) {
+        form.email.value = getItemData.email ;
+        form.message.value = getItemData.message ;
+    }
+    
+  }
+form.addEventListener("submit", onSummit);
+function onSummit(evt) {
+    evt.preventDefault()
+         const {
     elements: { email, message }
-    } = ent.currentTarget;
+    } = evt.currentTarget;
     const data = {
         email: email.value,
         message: message.value,
     };
-    localStorage.setItem('feedback-form-state', JSON.stringify(data));
-    }
+    console.log(data)
+    evt.currentTarget.reset();
+    localStorage.clear()
+    
+    
+
+  }
